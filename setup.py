847bf331db4b05a31e16728a93a19023577eb3b0
Fabian Reister <fabian.reister92@gmail.com>
2019-09-17 11:45:08 +0200

    Adding basic setup.py ; TODO: list dependencies

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='home_bank_converter',
+    version='',
+    packages=['home_bank_converter'],
+    url='',
+    license='',
+    author='Fabian Reister',
+    author_email='',
+    description=''
+)
