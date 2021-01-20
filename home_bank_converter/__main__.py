509710599444b1220961bd0ece68499b2fde8fd3
Fabian Reister <fabian.reister92@gmail.com>
2021-01-20 17:10:34 +0100

    logging, parser.parse with format hint

--- home_bank_converter/__main__.py
+++ home_bank_converter/__main__.py
@@ -1,17 +1,24 @@
 import argparse
+import logging
+import coloredlogs
+
 from home_bank_converter.converter import Converter
+from home_bank_converter.csv_file_format import csv_file_format_registry
 
 
 def main():
     parser = argparse.ArgumentParser(
         description="Convert a CSV export file from your online banking to a HomeBank compatible CSV format.")
     parser.add_argument("filename", help="The CSV file to convert.")
+    parser.add_argument("--format", type=str, default=None, choices=csv_file_format_registry.list)
     args = parser.parse_args()
 
     csv_parser = Converter(filename=args.filename)
-    csv_parser.parse()
+    csv_parser.parse(args.format)
     csv_parser.convert_and_write()
 
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    coloredlogs.install(level=logging.INFO)
     main()
