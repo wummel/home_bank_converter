efcca66dd76e3ff3a81cc282ccf47f7fa5907a2d
Fabian Reister <fabian.reister92@gmail.com>
2021-01-20 17:10:22 +0100

    adding deps to setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,15 @@ from setuptools import setup
 
 setup(
     name='home_bank_converter',
-    version='0.1.0',
+    version='0.2.0',
     packages=['home_bank_converter'],
     url='https://github.com/FabianReister/home_bank_converter',
     license='GPL-3.0',
     author='Fabian Reister',
     author_email='',
-    description='Converter for various online banking transaction files to be able to import them to HomeBank (http://homebank.free.fr)'
+    description='Converter for various online banking transaction files to be able to import them to HomeBank (http://homebank.free.fr)',
+    install_requires=[
+        "datetime",
+        "typing",
+    ]
 )
