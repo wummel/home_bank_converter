e056d5fe63cc45ee09cd52d8b9c38b083fbf7249
Philip Rinn <rinni@inventati.org>
2024-10-12 12:15:25 +0200

    Add 1822direkt as dialect

--- home_bank_converter/csv_dialects.py
+++ home_bank_converter/csv_dialects.py
@@ -27,7 +27,15 @@ class DialectSparkasse(csv.Dialect):
     lineterminator = '\r\n'
     quoting = csv.QUOTE_MINIMAL
 
+class DialectAchtzehnZweiundzwanzig(csv.Dialect):
+    delimiter = ';'
+    quotechar = '"'
+    doublequote = False
+    skipinitialspace = False
+    lineterminator = '\r\n'
+    quoting = csv.QUOTE_MINIMAL
 
 csv.register_dialect("dkb", DialectDKB)
 csv.register_dialect("vb", DialectVB)
 csv.register_dialect("sparkasse", DialectSparkasse)
+csv.register_dialect("1822direkt", DialectAchtzehnZweiundzwanzig)
